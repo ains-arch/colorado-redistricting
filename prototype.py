b8bc3b8a4dcffdc80246b438d57a7885008f44c3
ains-arch <ainsleearchibald@gmail.com>
2024-10-15 15:48:24 -0600

    the bit continues

--- prototype.py
+++ prototype.py
@@ -31,8 +31,13 @@ print("Information at Nodes: ", graph.nodes()[0].keys())
 # 'H_2MORE', 'VAP', 'HVAP', 'WVAP', 'BVAP', 'AMINVAP', 'ASIANVAP', 'NHPIVAP', 'OTHERVAP',
 # '2MOREVAP', 'geometry'])
 
-# CD116FP: 2016 congressional districts
-# TOTPOP: total population
+# CD116FP: Congressional district ID
+# REG18D: Number of registered Democratic voters for 2018 general election
+# REG18R: Number of registered Republican voters for 2018 general election
+# USH18D: Number of votes for 2018 Democratic US house candidates
+# USH18R: Number of votes for 2018 Republican US house candidates
+# TOTPOP: Total population
+# HISP: Hispanic population
 
 tot_pop = sum(graph.nodes()[v]['TOTPOP'] for v in graph.nodes())
 print("Total Population: ", tot_pop)
@@ -43,16 +48,16 @@ print("Geodataframe")
 gdf = gpd.read_file(PATH)
 print(f"shapefile columns: {gdf.columns}")
 
-# Plot by non-hispanic black population
-gdf['nh_black_percentage'] = gdf['NH_BLACK']/gdf['TOTPOP']
+# Plot by Hispanic percentage from the 2010 Census
+gdf['hisp_perc'] = gdf['HISP']/gdf['TOTPOP']
 plt.figure()
-gdf.plot(column = 'nh_black_percentage')
-plt.savefig("figs/nh-black.jpg")
-print("saved figs/nh-black.jpg")
+gdf.plot(column = 'hisp_perc')
+plt.savefig("figs/hispanic.jpg")
+print("saved figs/hispanic.jpg")
 
-# Plot by party preference
-gdf['party_preference'] = np.where(gdf['USH18D'] > gdf['USH18R'], "Democrat", "Republican")
-gdf.plot(column = 'party_preference', cmap = 'seismic')
+# Plot by party registration in 2018
+gdf['party_reg'] = np.where(gdf['REG18D'] > gdf['REG18R'], "Democrat", "Republican")
+gdf.plot(column = 'party_reg', cmap = 'seismic')
 plt.savefig("figs/party.jpg")
 print("saved figs/party.jpg")
 print("\n")
@@ -63,11 +68,11 @@ graph = Graph.from_geodataframe(gdf)
 print(f"Information at nodes: {graph.nodes()[0].keys()}")
 
 # Make a districting plan, using the "CD116FP" column
-cd16_plan = GeographicPartition(graph, assignment= "CD116FP")
+true_plan = GeographicPartition(graph, assignment= "CD116FP")
 
 # Draw the map
 plt.figure()
-gdf.plot(pd.Series([cd16_plan.assignment[i] for i in gdf.index]),
+gdf.plot(pd.Series([true_plan.assignment[i] for i in gdf.index]),
         cmap="tab20", figsize=(16,8))
 plt.savefig("figs/congressional-districts.jpg")
 print("saved figs/congressional-districts.jpg")
@@ -76,7 +81,7 @@ print("\n")
 # Make an initial districting plan using recursive_tree_part
 print("Initial districting plan")
 NUM_DIST = 7
-print(f"NUM_DIST set to {7} for the seven US House districts Colorado had in 2018")
+print(f"NUM_DIST set to {7} for the seven Congressional districts Colorado had in 2018")
 ideal_pop = tot_pop/NUM_DIST
 POP_TOLERANCE = 0.02
 initial_plan = recursive_tree_part(graph,
@@ -102,46 +107,46 @@ nx.draw(graph, node_size = 10, node_color = [initial_plan[v] for v in graph.node
 plt.savefig("figs/initial-plan.jpg")
 print("saved figs/initial-plan.jpg")
 
-# Count the cut edges of the partition
+# Count the cut edges of our initial plan
 initial_cutedges = 0
 for e in graph.edges():
     if initial_plan[e[0]] != initial_plan[e[1]]:
         initial_cutedges += 1
-print("Number of cutedges in initial_plan: ", initial_cutedges)
+print("Number of cutedges in our initial plan:", initial_cutedges)
 
-# Compare to the number of cut edges in the 2016 plan CD116FP
-cd16_cutedges = 0
+# Compare to the number of cut edges in the true Congressional districts
+cd_cutedges = 0
 for e in graph.edges():
     if graph.nodes()[e[0]]["CD116FP"] != graph.nodes()[e[1]]["CD116FP"]:
-        cd16_cutedges += 1
-print("Number of cutedges in CD116FP Plan: ", cd16_cutedges)
+        cd_cutedges += 1
+print("Number of cutedges in true congressional district plan:", cd_cutedges)
 
 # Find the population of each district:
 dist_pops = [0]*NUM_DIST
 for v in graph.nodes():
     dist = initial_plan[v]
     dist_pops[dist] = dist_pops[dist] + graph.nodes()[v]['TOTPOP']
-print("Populations of Districts: ", dist_pops)
+print("Populations of districts in our initial plan:", dist_pops)
 
 # Set up partition object
 initial_partition = Partition(
     graph, # dual graph
     assignment = initial_plan, # initial districting plan
     updaters = {
-        "our cut edges": cut_edges, 
-        "district population": Tally("TOTPOP", alias = "district population"), 
-        "district BPOP": Tally("NH_BLACK", alias = "district BPOP"), # missing hispanic-black
-        "US House R Votes": Tally("USH18R", alias = "US House R Votes"),
-        "US House D Votes": Tally("USH18D", alias = "US House D Votes")
+        "cutedges": cut_edges, 
+        "population": Tally("TOTPOP", alias = "population"), 
+        "Hispanic population": Tally("HISP", alias = "Hispanic population"),
+        "R registration": Tally("REG18R", alias = "R registration"),
+        "D registration": Tally("REG18D", alias = "D registration")
     }
 )
 
-# Districts with more Democratic votes that Republican votes
+# Districts with more Democratic voters than Republican voters
 d = 0
 for i in range(NUM_DIST):
-    if initial_partition["US House R Votes"][i] < initial_partition["US House D Votes"][i]:
+    if initial_partition["R registration"][i] < initial_partition["D registration"][i]:
         d = d + 1
-print("Number of Democratic Districts: ", d)
+print("Number of Democratic Districts:", d)
 
 # Set up random walk
 print("\n")
@@ -157,7 +162,7 @@ rw_proposal = partial(recom,                   # How you choose a next districti
 population_constraint = constraints.within_percent_of_ideal_population(
         initial_partition,
         POP_TOLERANCE,
-        pop_key = "district population"
+        pop_key = "population"
         )
 
 # Set up the chain
@@ -170,30 +175,94 @@ our_random_walk = MarkovChain(
         )
 
 # Run the random walk
-r_ensemble = []
+cutedge_ensemble = []
+hmaj_ensemble = []
+d_ensemble = []
+
 for part in our_random_walk:
-    # Districts with more R votes than D votes for US House in the 2018 midterm election
-    r_votes = 0
+    # Add cutedges to cutedges ensemble
+    cutedge_ensemble.append(len(part["cutedges"]))
+
+    # Hispanic-majority districts from US Census
+    hisp_maj = 0
     for i in range(NUM_DIST):
-        if part["US House R Votes"][i] > part["US House D Votes"][i]:
-            r_votes += 1
-    r_ensemble.append(r_votes)
+        h_perc = part["Hispanic population"][i]/part["population"][i]
+        if h_perc >= 0.5:
+            hisp_maj += 1
+    hmaj_ensemble.append(hisp_maj)
+
+    # Districts with more D voters than R voters in 2018
+    d_voters = 0
+    for i in range(NUM_DIST):
+        if part["D registration"][i] > part["R registration"][i]:
+            d_voters += 1
+    d_ensemble.append(d_voters)
+
 print("Ensemble generation complete")
 print("\n")
 
-# Histogram of number of Republican Districts from the 2018 US House Election
+# Histogram of number of cutedges in 2018 voting precincts
 plt.figure()
-plt.hist(r_ensemble)
-plt.savefig("figs/histogram-house-republicans.jpg")
-print("saved figs/histogram-house-republicans.jpg")
+plt.hist(cutedge_ensemble)
+plt.savefig("figs/histogram-cutedges.jpg")
+print("saved figs/histogram-cutedges.jpg")
+
+# Histogram of number of Hispanic-majority districts from 2010 Census numbers
+plt.figure()
+plt.hist(hmaj_ensemble)
+plt.savefig("figs/histogram-hispanic.jpg")
+print("saved figs/histogram-hispanic.jpg")
 
 # Specify boundaries between bins to make plot look a bit nicer
+# plt.figure()
+# plt.hist(hmaj_ensemble, bins=[0.5, 1.5, 2.5, 3.5, 4.5], edgecolor='black', color='orange')
+# plt.xticks([1, 2, 3, 4])
+# plt.xlabel("Hispanic majority districts", fontsize=12)
+# plt.ylabel("Ensembles", fontsize=12)
+# plt.title("Histogram of Hispanic-majority Districts by 2010 Census data",
+          # fontsize=14)
+# plt.savefig("figs/histogram-hispanic-clean.jpg", bbox_inches='tight')
+# print("saved figs/histogram-hispanic-clean.jpg. You should double check the bins.")
+
+# Histogram of number of Democratic-majority districts from the 2018 voter registration numbers
 plt.figure()
-plt.hist(r_ensemble, bins=[0.5, 1.5, 2.5, 3.5, 4.5], edgecolor='black', color='red')
-plt.xticks([1, 2, 3, 4])
-plt.xlabel("Republican majority districts", fontsize=12)
-plt.ylabel("Ensembles", fontsize=12)
-plt.title("Histogram of Republican Districts by Votes in the 2018 US House Election",
-          fontsize=14)
-plt.savefig("figs/histogram-house-republicans-clean.jpg", bbox_inches='tight')
-print("saved figs/histogram-house-republicans-clean.jpg. You should double check the bins.")
+plt.hist(d_ensemble)
+plt.savefig("figs/histogram-democrats.jpg")
+print("saved figs/histogram-democrats.jpg")
+
+# Specify boundaries between bins to make plot look a bit nicer
+# plt.figure()
+# plt.hist(d_ensemble, bins=[0.5, 1.5, 2.5, 3.5, 4.5], edgecolor='black', color='blue')
+# plt.xticks([1, 2, 3, 4])
+# plt.xlabel("Democratic-majority districts", fontsize=12)
+# plt.ylabel("Ensembles", fontsize=12)
+# plt.title("Histogram of Democratic-majority districts by registered voters in the 2018 Midterm
+# Elections",
+          # fontsize=14)
+# plt.savefig("figs/histogram-democrats-clean.jpg", bbox_inches='tight')
+# print("saved figs/histogram-democrats-clean.jpg. You should double check the bins.")
+
+PATH_21 = "data/2021_Approved_Congressional_Plan_with_Final_Adjustments/2021_Approved_Congressional_Plan_w_Final_Adjustments.shp"
+
+# Dual graph from shapefile
+print("2021 Graph")
+graph_21 = Graph.from_file(PATH_21)
+
+print("Is this dual graph connected? ", nx.is_connected(graph_21))
+print("Is this dual graph planar? ", nx.is_planar(graph_21))
+print("Number of Nodes: ", len(graph_21.nodes()))
+print("Number of Edges: ", len(graph_21.edges()))
+
+print("Information at Nodes: ", graph_21.nodes()[0].keys())
+# dict_keys(['boundary_node', 'area', 'OBJECTID', 'District', 'Shape_Leng', 'Shape_Le_1',
+# 'Shape_Area', 'geometry'])
+
+# Geodataframe from shapefile
+print("2021 Geodataframe")
+gdf_21 = gpd.read_file(PATH_21)
+print(f"shapefile columns: {gdf_21.columns}")
+
+# Plot by party preference
+plt.figure()
+gdf_21.plot(cmap = 'tab10')
+plt.savefig("figs/2021.jpg")
--- writeup.md
+++ writeup.md
@@ -2,14 +2,42 @@
 Ensemble analysis of Colorado's congressional districting plan based on data from 2018 midterm
 elections. Colorado had 7 US House districts in 2018.
 
+Due to availability of data, this ensemble analysis focuses on
+1. the US House map in 2018 compiled by MGGG
+    1. 2018 Colorado precinct shapefile from Todd Bleess of the Colorado State Demographer's Office
+    1. election data from the Colorado Secretary of State's Office
+    1. 2010 Decennial Census demographic data
+1. information about the 2012 redistricting processes from Ballotpedia
+
 # Background
-How many districts? 7
+## How many districts?
+7 congressional seats, made up of Census enumeration districts
 
-Who draws them?
+## Who draws them?
+Colorado General Assembly
+committee comprised of members of both chambers with equal split in party membership
+With staff members, it was a 5-5-1 R-D-I split.
+Governor of Colorado (Democrat John Hickenlooper) had veto power over the bills
+Ultimately, because the Assembly failed to pass a map by the deadline, the redistricting process
+went to court.
 
-Legal challenges?
+## Legal challenges?
+The redistricting process in the legislative session was a mess that failed to reach any common
+ground on maps by Sine Die. Both major parties filed suit, as well as the Colorado Hispanic Bar
+Association and Colorado Latino Forum. Cases were consolidated, deadlines for maps from the major
+parties and other groups on the lawsuit were set. The court considered a half dozen maps and
+approved the Moreno/South Democratic map, praising it for maximizing competitiveness, current
+communities of interest, and "the real possibility that voters will be as engaged in the electoral
+process as possible."
 
-Redrawing of lines recently?
+Republicans filed an appeal, but the ruling was upheld in the state Supreme Court, dismissing the
+argument that the map was bad because it cut up too many counties into multiple districts. The final
+map created three competitive Congressional races. Legislative reform was proposed after the
+redistricting process was completed.
+
+## Redrawing of lines recently?
+The lines have since been redrawn after the 2020 census to reflect Colorado's new eighth
+representative.
 
 # Findings
 jpgs go here
