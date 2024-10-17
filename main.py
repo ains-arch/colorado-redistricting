9509f59d31988ec9f7ab91f0b8d5d7c03147c8d2
ains-arch <ainsleearchibald@gmail.com>
2024-10-16 22:02:28 -0700

    new and improved code

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ hisp = sum(graph.nodes()[v]['HISP'] for v in graph.nodes())
 print(f"Hispanic Population: {hisp}")
 totpop = sum(graph.nodes()[v]['TOTPOP'] for v in graph.nodes())
 print(f"Total Population: {totpop}")
-print(f"Hispanic percentage: {hisp/totpop:.2%}\n")
+print(f"Hispanic percentage: {hisp/totpop:.2%}")
 
 # Add Hispanic percentage from the 2010 Census to the geodataframe
 gdf['hisp_perc'] = gdf['HISP']/gdf['TOTPOP']
@@ -69,7 +69,7 @@ cbar = plt.colorbar(sm, ax=ax, orientation='horizontal', pad=-0.01, shrink=0.7)
 plt.axis('off') # To suppress axes in the map plot
 plt.title('Hispanic Percentage in 2010 Census', fontsize=14)
 plt.savefig("figs/hispanic.png")
-print("Saved figs/hispanic.png")
+print("\nSaved figs/hispanic.png")
 
 # Add Democratic votes percentage in 2018 US House race to the geodataframe
 gdf['dem_perc'] = gdf["USH18D"]/gdf[["USH18D", "USH18R"]].sum(axis=1)
@@ -86,81 +86,54 @@ cbar.set_ticklabels(['Republican', 'Democratic'])  # Set custom labels
 plt.axis('off') # To suppress axes in the map plot
 plt.title('Votes for US House in 2018 Midterms', fontsize=14)
 plt.savefig("figs/dem.png")
-print("saved figs/dem.png")
+print("Saved figs/dem.png\n")
 
 # Redefine new graph object with the hisp_perc and dem_perc columns
 # print("Loading updated graph...")
-graph = Graph.from_geodataframe(gdf)
+# graph = Graph.from_geodataframe(gdf)
 # print("Updated graph loaded.")
 # print(f"Information at nodes: {graph.nodes()[0].keys()}")
 
 # Create a partition object from the enacted "CD116FP" districting plan
-print("Turn enacted congressional map into a partition")
+print("Turning enacted congressional map into a partition...")
 enacted_plan = GeographicPartition(graph,
                                 assignment= "CD116FP",
                                 updaters = {
                                     "cutedges": cut_edges, 
-                                    "population": Tally("TOTPOP", alias = "population"), 
-                                    "Hispanic population": Tally("HISP",
-                                                                 alias = "Hispanic population"),
-                                    "R votes": Tally("USH18R", alias = "R votes"),
-                                    "D votes": Tally("USH18D", alias = "D votes")
                                     }
                                 )
 
-# Calculate enacted number of 30%+ Hispanic districts from US Census
-hisp_30_enacted = 0
-for i in enacted_plan.parts:
-    h_perc_enacted = enacted_plan["Hispanic population"][i]/enacted_plan["population"][i]
-    if h_perc_enacted >= 0.3:
-        hisp_30_enacted += 1
+print("Running aggregation into congressional districts...")
+# Aggregate data using groupby
+district_stats = gdf.groupby('CD116FP').agg({
+    'TOTPOP': 'sum',
+    'HISP': 'sum',
+    'USH18D': 'sum',
+    'USH18R': 'sum'
+}).reset_index()
+
+# Calculate percentage of Hispanic population and Democratic votes
+district_stats['hisp_perc_cd'] = district_stats['HISP'] / district_stats['TOTPOP']
+district_stats['dem_perc_cd'] = district_stats['USH18D'] / (district_stats['USH18D'] + district_stats['USH18R'])
+
+# Calculate the number of 30%+ Hispanic districts
+hisp_30_enacted = (district_stats['hisp_perc_cd'] >= 0.3).sum()
 print(f"Number of districts with 30% or more Hispanic population: {hisp_30_enacted}")
 
-# Calculate enacted number of Democratic districts in 2018 Congressional race
-d_votes_enacted = 0
-for i in enacted_plan.parts:
-    if enacted_plan["D votes"][i] > enacted_plan["R votes"][i]:
-        d_votes_enacted += 1
+# Calculate the number of Democratic districts
+d_votes_enacted = (district_stats['USH18D'] > district_stats['USH18R']).sum()
 print(f"Number of districts Democrats won in 2018 House elections: {d_votes_enacted}")
 
-# Calculate true number of cutedges
+# Calculate number of cutedges
 cutedges_enacted = len(enacted_plan['cutedges'])
 print(f"Number of cutedges: {cutedges_enacted}")
 
-### Begin code yoinked from ChatGPT ###
+# Merge the aggregated data back into the GeoDataFrame
+gdf = gdf.merge(district_stats[['CD116FP', 'hisp_perc_cd', 'dem_perc_cd']], on='CD116FP', how='left')
 
-# Step 1: Aggregate data for each congressional district in enacted_plan
+print("Enacted congressional map data manipulation complete.\n")
 
-# Create empty dictionaries to hold totals per district
-total_pop_by_cd = {cd: 0 for cd in set(enacted_plan.assignment.values())}
-hisp_pop_by_cd = {cd: 0 for cd in set(enacted_plan.assignment.values())}
-dem_votes_by_cd = {cd: 0 for cd in set(enacted_plan.assignment.values())}
-total_votes_by_cd = {cd: 0 for cd in set(enacted_plan.assignment.values())}
-
-# Loop through each census block and assign its values to its congressional district
-for index, row in gdf.iterrows():
-    cd = enacted_plan.assignment[index]  # Congressional district ID
-    total_pop_by_cd[cd] += row['TOTPOP']  # Total population
-    hisp_pop_by_cd[cd] += row['HISP']  # Hispanic population
-    dem_votes_by_cd[cd] += row['USH18D']  # Democratic votes
-    # Due to data availability, make the assumption
-    # that total votes = the votes cast for the two major parties
-    # This will allow the major party vote percentage to be plotted on a blue-red scale
-    # And maintain 0.5 as the tipping point for which candidate won
-    # (Which itself also makes the assumption
-    # that no minor party candidate could win a plurality of the votes)
-    total_votes_by_cd[cd] += row['USH18D'] + row['USH18R']
-
-# Step 2: Calculate percentages by congressional district
-hisp_perc_by_cd = {cd: hisp_pop_by_cd[cd] / total_pop_by_cd[cd]
-                   if total_pop_by_cd[cd] > 0 else 0
-                   for cd in total_pop_by_cd}
-dem_perc_by_cd = {cd: dem_votes_by_cd[cd] / total_votes_by_cd[cd]
-                  if total_votes_by_cd[cd] > 0 else 0
-                  for cd in total_votes_by_cd}
-
-# Step 3: Plot the Hispanic percentage by congressional district
-gdf['hisp_perc_cd'] = gdf.index.map(lambda i: hisp_perc_by_cd[enacted_plan.assignment[i]])
+# Plot the Hispanic percentage by congressional district
 plt.figure()
 # Use a scale of 0 to 0.3 even though no values are at 0.3 to indicate falling below 30%
 # This value will become important in the ensemble analysis
@@ -173,10 +146,9 @@ plt.title('Hispanic Percentage in 2010 Census by Congressional District', fontsi
 plt.savefig("figs/hispanic_cd.png")
 print("Saved figs/hispanic_cd.png")
 
-# Step 4: Plot the partisan vote makeup by congressional district
-gdf['party_cd'] = gdf.index.map(lambda i: dem_perc_by_cd[enacted_plan.assignment[i]])
+# Plot the partisan vote makeup by congressional district
 plt.figure()
-ax = gdf.plot(column='party_cd', cmap='seismic_r', vmin=0, vmax=1)
+ax = gdf.plot(column='dem_perc_cd', cmap='seismic_r', vmin=0, vmax=1)
 plt.axis('off') # To suppress axes in the map plot
 plt.title('Votes for US House in 2018 Midterms by Congressional District', fontsize=14)
 sm = plt.cm.ScalarMappable(cmap='seismic_r', norm=plt.Normalize(vmin=0, vmax=1)) # Custom legend
@@ -188,23 +160,17 @@ cbar.set_ticklabels(['Republican', 'Democratic'])  # Set custom labels
 plt.savefig("figs/party_cd.png")
 print("Saved figs/party_cd.png")
 
-# TODO: should be able to do this with a groupby
-# TODO: or maybe some kind of join situation
-
-### End code yoinked from ChatGPT ###
-
 # Plot congressional districts with no coloring
 plt.figure()
 gdf.plot(pd.Series([enacted_plan.assignment[i] for i in gdf.index]), cmap="tab10")
 plt.title('Congressional Districts in 2018', fontsize=14)
 plt.axis('off')
 plt.savefig("figs/2012-congressional-districts.png")
-print("saved figs/2012-congressional-districts.png")
+print("Saved figs/2012-congressional-districts.png\n")
 
 # Make an initial districting plan using recursive_tree_part
-# print("Initial districting plan")
-NUM_DIST = 7
-# print(f"NUM_DIST set to {7} for the seven Congressional districts Colorado had in 2018")
+print("Setting up ensemble..")
+NUM_DIST = 7 # for the seven Congressional districts Colorado had in 2018
 ideal_pop = totpop/NUM_DIST
 POP_TOLERANCE = 0.02
 initial_plan = recursive_tree_part(graph,
@@ -231,23 +197,12 @@ initial_partition = Partition(
         "cutedges": cut_edges, 
         "population": Tally("TOTPOP", alias = "population"), 
         "Hispanic population": Tally("HISP", alias = "Hispanic population"),
-        # "R registration": Tally("REG18R", alias = "R registration"),
-        # "D registration": Tally("REG18D", alias = "D registration"),
         "R votes": Tally("USH18R", alias = "R votes"),
         "D votes": Tally("USH18D", alias = "D votes")
     }
 )
 
-# Districts with more Democratic voters than Republican voters
-# d = 0
-# for i in range(NUM_DIST):
-    # if initial_partition["R registration"][i] < initial_partition["D registration"][i]:
-        # d = d + 1
-# print("Number of Democratic Districts:", d)
-
 # Set up random walk
-print("\n")
-print("Begin ensemble generation")
 rw_proposal = partial(recom,                   # How you choose a next districting plan
                       pop_col = "TOTPOP",      # What data describes population
                       pop_target = ideal_pop,  # Target/ideal population is for each district
@@ -272,6 +227,7 @@ our_random_walk = MarkovChain(
         )
 
 # Run the random walk
+print("Running random walk...")
 cutedge_ensemble = []
 h30_ensemble = []
 d_ensemble = []
@@ -295,8 +251,7 @@ for part in our_random_walk:
             d_votes += 1
     d_ensemble.append(d_votes)
 
-print("Ensemble generation complete")
-print("\n")
+print("Random walk complete.\n")
 
 # Histogram of number of cutedges in 2018 voting precincts
 plt.figure()
@@ -308,13 +263,13 @@ plt.title("Cutedges in 2018 Voting Precincts",
 plt.axvline(x=cutedges_enacted, color='orange', linestyle='--', linewidth=2,
             label=f'cutedges_enacted = {cutedges_enacted}')
 plt.savefig("figs/histogram-cutedges.png")
-print("saved figs/histogram-cutedges.png")
+print("Saved figs/histogram-cutedges.png")
 
 # Histogram of number of Hispanic-30%+ districts from 2010 Census numbers
 plt.figure()
 plt.hist(h30_ensemble)
 plt.savefig("figs/histogram-hispanic.png")
-print("saved figs/histogram-hispanic.png")
+print("Saved figs/histogram-hispanic.png")
 
 # Specify boundaries between bins to make plot look a bit nicer
 plt.figure()
@@ -327,13 +282,13 @@ plt.axvline(x=hisp_30_enacted, color='blue', linestyle='--', linewidth=2,
 plt.title("Districts with >30% Hispanic Population in 2010 Census",
           fontsize=14)
 plt.savefig("figs/histogram-hispanic-clean.png")
-print("saved figs/histogram-hispanic-clean.png. You should double check the bins.")
+print("Saved figs/histogram-hispanic-clean.png. You should double check the bins.")
 
 # Histogram of number of Democratic districts in US House race
 plt.figure()
 plt.hist(d_ensemble)
 plt.savefig("figs/histogram-democrats.png")
-print("saved figs/histogram-democrats.png")
+print("Saved figs/histogram-democrats.png")
 
 # Specify boundaries between bins to make plot look a bit nicer
 plt.figure()
@@ -345,7 +300,7 @@ plt.axvline(x=d_votes_enacted, color='orange', linestyle='--', linewidth=2,
             label=f'd_votes_enacted = {d_votes_enacted}')
 plt.title("Democratic Districts in the 2018 Midterm Elections", fontsize=14)
 plt.savefig("figs/histogram-democrats-clean.png")
-print("saved figs/histogram-democrats-clean.png. You should double check the bins.")
+print("Saved figs/histogram-democrats-clean.png. You should double check the bins.")
 
 # TODO: make boxplots
 # TODO: make a series of histograms to show approaching stationary distribution
@@ -381,4 +336,4 @@ plt.axis('off')
 plt.xticks([])
 plt.yticks([])
 plt.savefig("figs/2021-congressional-districts.png")
-print("saved figs/2021-congressional-districts.png")
+print("Saved figs/2021-congressional-districts.png")
